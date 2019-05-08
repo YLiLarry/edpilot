8633394863438bbf6dcf529648a63386de7c7152
Yu Li <YLiLarry@gmail.com>
2019-05-08 17:08:17 -0400

    upload

--- src/edpilot.cc
+++ src/edpilot.cc
@@ -13,13 +13,7 @@ using namespace edpilot;
 using namespace ggframe;
 
 #if APPLE
-using boost::filesystem::path;
-using boost::filesystem::is_directory;
-using boost::filesystem::directory_iterator;
-#else
-using std::filesystem::path;
-using std::filesystem::is_directory;
-using std::filesystem::directory_iterator;
+using namespace boost;
 #endif
 
 void SpeedObserver::trainOnline(vector<FrameFeature> const& features, vector<unsigned> const& speed)
@@ -31,56 +25,55 @@ void SpeedObserver::labelTrainingDataInteractive()
 {
 
 	path data_dir;
-	do {
-		std::cout << "Where can I find the images?" << std::endl;
-		string input;
-		std::getline(std::cin, input);
-		if (input.length() == 0) {
+	/* define where data is located */
 #if WIN32
-			input = "C:\\Users\\Yu\\Desktop\\data";
+	data_dir = "C:\\Users\\Yu\\Desktop\\data";
 #endif
 #if APPLE
-			input = "/Users/yuli/Desktop/data";
+	data_dir = "/Users/yuli/Downloads/data";
 #endif
-		} else if (input.length() && input[0] == '"')
-		{
-			input = input.substr(1, input.length() - 2);
-		}
-		data_dir = input;
-		std::cout << "checking " << data_dir << std::endl;
-	} while (! is_directory(data_dir));
+	std::cerr << "checking " << data_dir << std::endl;
+	assert(filesystem::is_directory(data_dir));
 	Frame pattern;
-	for (auto& file : directory_iterator(data_dir))
+	filesystem::path template_file = data_dir;
+	template_file /= "template.bmp";
+	if (filesystem::exists(template_file)) {
+		pattern.load(template_file);
+	}
+	for (auto& file : filesystem::directory_iterator(data_dir))
 	{
 		string fp = file.path().string();
-		if (fp.find(".bmp") != fp.npos) {
-			std::cout << "found img " << fp << std::endl;
+		if (fp.find(".bmp") != fp.npos && file.path().filename() != "template.bmp") {
+			std::cerr << "found img " << fp << std::endl;
 			ggframe::Frame frame(fp);
-			if (! pattern.empty()) {
-				cout << "trying to match pattern" << endl;
-				Rec matched_rec = frame.findPattern(pattern);
-				frame.drawRec(matched_rec);
-				frame.display();
-			}
-			std::cout << "where is the interested region?" << std::endl;
 			frame.setGridSize(10);
-			frame.display();
-			InputEvent event = frame.waitForInput();
+			InputEvent event;
 			Rec mouse_selection;
-			while (event.source == InputSource::Mouse)
-			{
-				frame.load(fp);
-				mouse_selection = frame.bestGridRecCenteredAt(Pos::rc(event.mouse.row(), event.mouse.col()), ggframe::Size::hw(180, 100));
-				frame.drawRec(mouse_selection);
-				frame.display();
-				event = frame.waitForInput();
-			}
 			if (pattern.empty()) {
+				std::cerr << "where is the interested region?" << std::endl;
+				Frame tmp = frame;
+				do {
+					tmp.display();
+					event = tmp.waitForInput();
+					mouse_selection = frame.bestGridRecCenteredAt(Pos::rc(event.mouse.row(), event.mouse.col()), ggframe::Size::hw(180, 100));
+					tmp.drawRec(mouse_selection);
+				} while (event.source == InputSource::Mouse && pattern.empty());
 				pattern = frame;
 				pattern.crop(mouse_selection);
-			}
-			if (event.type == InputEventType::WindowClose) {
-				return;
+				cerr << "where should I save the pattern?" << endl;
+				filesystem::path template_dir = data_dir;
+				template_dir /= "template.bmp";
+				pattern.save(template_dir);
+			} else {
+				cerr << "trying to match pattern" << endl;
+				Rec matched_rec = frame.recMatchedTemplate(pattern);
+				// frame.drawRec(matched_rec);
+				// frame.display();
+				// cerr << "showing matched rectangle" << endl;
+				InputEvent e;
+				while(e.source != Keyboard) {
+					e = frame.waitForInput();
+				}
 			}
 		}
 	}
